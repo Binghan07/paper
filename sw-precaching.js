83c6fa43db8b86e662823980a6fbf9117bbdb2f3
Binghan07 <58183910+Binghan07@users.noreply.github.com>
2020-10-20 02:46:20 +0800

    v1.8

--- sw-precaching.js
+++ sw-precaching.js
@@ -1,25 +1,21 @@
-console.log('Hello from sw.js')
+console.log('Hello PWA')
 
 //////////////////////////////////////////////////////////////////////////////
-//		load workbox
+//		加載工作
 //////////////////////////////////////////////////////////////////////////////
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js')
 
 if (workbox) {
-	console.log(`Yay! Workbox is loaded 🎉`)
+	console.log(`加載成功 `)
 } else {
-	console.log(`Boo! Workbox didn't load 😬`)
+	console.log(`工作未加載 `)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-//		set log level
+//		設置級別
 //////////////////////////////////////////////////////////////////////////////
-// The most verbose - displays all logs.
 workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
-
-//////////////////////////////////////////////////////////////////////////////
-//		register some routes
-//////////////////////////////////////////////////////////////////////////////
+//		註冊
 workbox.routing.registerRoute(
 	new RegExp('.*\.js'),
 	workbox.strategies.cacheFirst()
@@ -31,25 +27,16 @@ workbox.routing.registerRoute(
 )
 
 
-// TODO register / for index.html
-// - is this needed ?
-
-//////////////////////////////////////////////////////////////////////////////
-//		precache and route
-//////////////////////////////////////////////////////////////////////////////
-
-// TO BE FILLED BY ``````
+//	預緩存url
+// 可填寫 
 workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
-    "revision": "cc28257303356890e7011a6a28167324"
   },
   {
     "url": "sw.js",
-    "revision": "d4b9dddffb8524988be9fc3e44af1c65"
   },
   {
-    "url": "vendor/aframe.min.js",
-    "revision": "50610178305c4ae36dce20d74d7dd06c"
+    "url": "main/aframe.min.js",
   }
 ])
